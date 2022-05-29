f6d690db0ee516dd5c42ebe57f3a89c9a64f421f
Joseph DeAngeloi <deangelo.joe@Gmail.com>
2022-05-29 08:41:31 -0400

    update

---
+++ scripts/deploy.js
@@ -0,0 +1,33 @@
+const compile = require('./compile.js');
+const ethers = require('ethers');
+require('dotenv').config();
+
+async function main() {
+
+  //copy-paste your Alchemy HTTP Rinkeby URL
+  const url = "https://eth-rinkeby.alchemyapi.io/v2/UO6mvOL5zd-YdMOVFS0ww0sOvUJM8b3Y";
+
+  let artifacts = compile("Faucet", "Faucet.sol");
+
+   const provider = new ethers.providers.JsonRpcProvider(url);
+
+   let privateKey = process.env.PRIVATE_KEY;
+
+   let wallet = new ethers.Wallet(privateKey, provider);
+
+   // Create an instance of a Faucet Factory
+   let factory = new ethers.ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
+
+   let faucet = await factory.deploy();
+
+   console.log("Faucet address:", faucet.address);
+
+   await faucet.deployed();
+ }
+
+ main()
+   .then(() => process.exit(0))
+   .catch(error => {
+     console.error(error);
+     process.exit(1);
+ });
